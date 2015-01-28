533cc4b7596e7b192aa30864987da5250ba13eff
Harshit Agarwal <harshitagarwal90@gmail.com>
2015-01-28 15:08:47 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Harshit Agarwal on 1/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
